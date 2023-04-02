0ff3a3d3aa376871c450f76b1772734473e3f3fd
Oleksandr Stetsiuk <oleksandr.v.stetsiuk@gmail.com>
2023-04-03 02:44:50 +0300

    d47, 48

---
+++ d47/app.js
@@ -0,0 +1,63 @@
+const fs = require("fs");
+const path = require("path");
+
+const express = require("express");
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/currenttime", function (req, res) {
+  res.send("<h1>" + new Date().toISOString() + "</h1>");
+});
+
+app.get("/", function (req, res) {
+  res.send(
+    '<form action="/store-user" method="POST"><label>Your Name</label><input type="text" name="username" /><button>Submit</button></form>'
+  );
+});
+
+app.post("/store-user", function (req, res) {
+  const userName = req.body.username;
+  const filePath = path.join(__dirname, "data", "users.json");
+  const fileData = fs.readFileSync(filePath);
+  const existingUsers = JSON.parse(fileData);
+  existingUsers.push(userName);
+  fs.writeFileSync(filePath, JSON.stringify(existingUsers));
+  res.send("<h1>Username sotred!</h1>");
+});
+
+app.get("/users", function (req, res) {
+  const filePath = path.join(__dirname, "data", "users.json");
+
+  const fileData = fs.readFileSync(filePath);
+  const existingUsers = JSON.parse(fileData);
+
+  let responseData = "<ul>";
+
+  for (const user of existingUsers) {
+    responseData += `<li>${user}</li>`;
+  }
+
+  responseData += "</ul>";
+
+  res.send(responseData);
+});
+
+app.listen(3000);
+
+// const http = require("express");
+
+// function handleRequest(request, response) {
+//   if (request.url === "/currenttime") {
+//     response.statusCode = 200;
+//     response.end("<h1>" + new Date().toISOString() + "</h1>");
+//   } else if (request.url === "/") {
+//     response.statusCode = 200;
+//     response.end("<h1>Hello World!</h1>");
+//   }
+// }
+
+// const server = http.createServer(handleRequest);
+
+// server.listen(3000);
